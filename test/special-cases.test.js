6117a11896d352ce590398e27b58cd2864b0a98e
Manuel Spigolon <behemoth89@gmail.com>
2021-03-27 19:23:48 +0100

    fix: surname support

---
+++ test/special-cases.test.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const { test } = require('tap')
+
+const parseChat = require('../lib/parser')
+
+test('name surname', t => {
+  t.plan(1)
+  const txt = `14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+14:26:15 Agent Fortyseven : Aliquam non cursus erat, ut blandit lectus.`
+  const parsed = parseChat(txt)
+  t.deepEqual(parsed, [{
+    date: '14:24:32',
+    mention: '14:24:32 Customer : ',
+    sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n',
+    type: 'customer'
+  }, {
+    date: '14:26:15',
+    mention: '14:26:15 Agent Fortyseven : ',
+    sentence: 'Aliquam non cursus erat, ut blandit lectus.',
+    type: 'agent'
+  }])
+})
+
+test('name nickname', t => {
+  t.plan(1)
+  const txt = `14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+14:26:15 Agent 47 : Aliquam non cursus erat, ut blandit lectus.`
+  const parsed = parseChat(txt)
+  t.deepEqual(parsed, [{
+    date: '14:24:32',
+    mention: '14:24:32 Customer : ',
+    sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n',
+    type: 'customer'
+  }, {
+    date: '14:26:15',
+    mention: '14:26:15 Agent 47 : ',
+    sentence: 'Aliquam non cursus erat, ut blandit lectus.',
+    type: 'agent'
+  }])
+})
+
+test('name surname missing colon', t => {
+  t.plan(1)
+  const txt = `14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+14:26:15 Agent Surname Aliquam non cursus erat, ut blandit lectus.`
+  const parsed = parseChat(txt)
+  t.deepEqual(parsed, [{
+    date: '14:24:32',
+    mention: '14:24:32 Customer : ',
+    sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n',
+    type: 'customer'
+  }, {
+    date: '14:26:15',
+    mention: '14:26:15 Agent ',
+    sentence: 'Surname Aliquam non cursus erat, ut blandit lectus.',
+    type: 'agent'
+  }])
+})
